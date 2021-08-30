e73441a636a7d99543143c929e02f92ca73d6193
Samuel <samlee1097@yahoo.com>
2021-08-30 13:30:01 -0400

    commit tree b7b6b4013ba7419d72927da80cdad3f7fc4e86ec

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    return Math.abs(42-start);
+};
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start)*264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination)*264;
+};
+
+function calculatesFarePrice(start, destination){ 
+    const distanceTraveled = distanceTravelledInFeet(start, destination);
+    if (distanceTraveled <= 400) {
+        return 0;
+    } else if (distanceTraveled > 400 && distanceTraveled <= 2000) {
+        return (distanceTraveled - 400)*0.02;
+    } else if (distanceTraveled > 2000 && distanceTraveled <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+};
